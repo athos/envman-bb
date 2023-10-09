a19a34edbf6aebfaf4f188be90b3667ea4f178b4
Shogo Ohta <athos0220@gmail.com>
2023-10-09 19:20:36 +0900

    Keep file creation time as it was before editing

--- src/envman/cmd/edit.clj
+++ src/envman/cmd/edit.clj
@@ -1,12 +1,15 @@
 (ns envman.cmd.edit
   (:require [envman.edit :as edit]
             [envman.files :as files]
-            [envman.util :as util]))
+            [envman.util :as util]
+            [babashka.fs :as fs]))
 
 (def opts-spec
   [[:name {:coerce util/check-name}]])
 
 (defn edit [{:keys [opts]}]
   (files/ensure-envman-dirs)
-  (let [fpath (files/envman-path (:name opts))]
-    (edit/edit :init-content (slurp fpath) :to fpath)))
+  (let [fpath (files/existing-envman-path (:name opts))
+        time-created (fs/creation-time fpath)]
+    (edit/edit :init-content (slurp fpath) :to fpath :force true)
+    (fs/set-creation-time fpath time-created)))
