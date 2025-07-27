7d7ab0b4414858bab2219f54522a3e80e056ee61
Shogo Ohta <athos0220@gmail.com>
2025-07-27 23:31:03 +0900

    Enable envsets to include other envsets

--- src/envman/cmd/run.clj
+++ src/envman/cmd/run.clj
@@ -21,54 +21,72 @@
            :coerce :boolean
            :alias :E}]])
 
-(defn- load-dotenv [env in]
-  (let [{:keys [meta body]} (envset/parse (slurp (fs/file in)))]
-    (if-let [script (:setup meta)]
-      (let [script-path (fs/create-temp-file {:suffix ".sh" :posix-file-permissions "rw-------"})
-            script-file (fs/file script-path)
-            env-file (fs/create-temp-file {:suffix ".env" :posix-file-permission "rw-------"})
-            env' (assoc env "ENVMAN_ENV" (str env-file))
-            boundary (str "---" (random-uuid) "---")]
-        (spit script-file script)
-        (spit script-file (str "echo\necho \"" boundary "\"\n") :append true)
-        (spit script-file "set\n" :append true)
-        (try
-          (let [{:keys [out]} @(proc/shell {:env env', :in script-file,
-                                            :out :string, :err :inherit}
-                                           "/bin/bash -s")
-                shell-vars (->> (str/split-lines out)
-                                (drop-while (partial not= boundary))
-                                rest
-                                (into {}
-                                      (map (fn [line]
-                                             (let [[_ k v] (re-matches #"([^=]+)=(.*)" line)]
-                                               [k v])))))
-                out-vars (dotenv/parse (slurp (fs/file env-file)))
-                env' (-> env (into shell-vars) (into out-vars))]
-            (-> shell-vars
-                (into out-vars)
-                (into (dotenv/expand-vars env' body))))
-          (finally
-            (fs/delete-if-exists env-file)
-            (fs/delete-if-exists script-path))))
-      (dotenv/expand-vars env body))))
+(defn- load-envset [env {:keys [meta body]}]
+  (if-let [script (:setup meta)]
+    (let [script-path (fs/create-temp-file {:suffix ".sh" :posix-file-permissions "rw-------"})
+          script-file (fs/file script-path)
+          env-file (fs/create-temp-file {:suffix ".env" :posix-file-permission "rw-------"})
+          env' (assoc env "ENVMAN_ENV" (str env-file))
+          boundary (str "---" (random-uuid) "---")]
+      (spit script-file script)
+      (spit script-file (str "echo\necho \"" boundary "\"\n") :append true)
+      (spit script-file "set\n" :append true)
+      (try
+        (let [{:keys [out]} @(proc/shell {:env env', :in script-file,
+                                          :out :string, :err :inherit}
+                                         "/bin/bash -s")
+              shell-vars (->> (str/split-lines out)
+                              (drop-while (partial not= boundary))
+                              rest
+                              (into {}
+                                    (map (fn [line]
+                                           (let [[_ k v] (re-matches #"([^=]+)=(.*)" line)]
+                                             [k v])))))
+              out-vars (dotenv/load (slurp (fs/file env-file)))
+              env' (-> env (into shell-vars) (into out-vars))]
+          (into out-vars (dotenv/expand-vars env' body)))
+        (finally
+          (fs/delete-if-exists env-file)
+          (fs/delete-if-exists script-path))))
+    (dotenv/expand-vars env body)))
+
+(defn- parse-file [f]
+  (envset/parse (slurp (fs/file f))))
+
+(defn- resolve-deps [paths envsets]
+  (letfn [(step [path {:keys [meta body]}]
+            (-> []
+                (into (mapcat #(step % (parse-file (files/name-path %))))
+                      (:include meta))
+                (conj {:path path, :meta meta, :body body})))]
+    (loop [[{:keys [path] :as e} & more] (mapcat step paths envsets)
+           seen #{}
+           ret []]
+      (if e
+        (if (seen path)
+          (recur more seen ret)
+          (recur more (conj seen path) (conj ret e)))
+        ret))))
 
 (defn- update-env [init-env paths vars]
-  (as-> {:env init-env :updated []} acc
-    (reduce (fn [acc path]
-              (let [vars (load-dotenv (:env acc) path)]
-                (-> acc
-                    (update :env into vars)
-                    (update :updated into (map first vars)))))
-            acc
-            paths)
-    (reduce (fn [acc var]
-              (let [[k v] (dotenv/parse1 var {:vars (:env acc) :allow-export? false})]
-                (-> acc
-                    (assoc-in [:env k] v)
-                    (update :updated conj k))))
-            acc
-            vars)))
+  (let [parsed-envsets (->> paths
+                            (map #(parse-file (files/name-path %)))
+                            (resolve-deps paths))]
+    (as-> {:env init-env :updated []} acc
+      (reduce (fn [acc parsed-envset]
+                (let [vars (load-envset (:env acc) parsed-envset)]
+                  (-> acc
+                      (update :env into vars)
+                      (update :updated into (map first vars)))))
+              acc
+              parsed-envsets)
+      (reduce (fn [acc var]
+                (let [[k v] (dotenv/parse1 var {:vars (:env acc) :allow-export? false})]
+                  (-> acc
+                      (assoc-in [:env k] v)
+                      (update :updated conj k))))
+              acc
+              vars))))
 
 (defn- quote-content [s]
   (str \"
@@ -88,7 +106,7 @@
   (let [content (with-out-str
                   (print-env edit-vars env))
         tmp (edit/edit :init-content content)
-        env' (into {} (load-dotenv env tmp))]
+        env' (into {} (load-envset env (dotenv/parse tmp)))]
     (fs/delete tmp)
     env'))
 
@@ -96,9 +114,8 @@
   (apply proc/shell {:inherit true :env env :continue true} cmd))
 
 (defn run [{:keys [opts args]}]
-  (let [paths (map files/name-path (:name opts))
-        init-env (into {} (System/getenv))
-        {:keys [env updated]} (update-env init-env paths (:env opts))
+  (let [init-env (into {} (System/getenv))
+        {:keys [env updated]} (update-env init-env (:name opts) (:env opts))
         env (cond->> (select-keys env updated)
               (:edit opts)
               (edit-and-load updated)
